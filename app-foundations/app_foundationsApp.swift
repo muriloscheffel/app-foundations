3303de60f3208bca0f9d6fbb76979c978018d556
Murilo Scheffel <murilo.scheffel@outlook.com>
2025-11-25 09:12:36 -0300

    Initial Commit

---
+++ app-foundations/app_foundationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_foundationsApp.swift
+//  app-foundations
+//
+//  Created by aluno-15 on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct app_foundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
