41cf4b392aeb7e01afa6fe3fb5786475fe5c9371
Murilo Scheffel <murilo.scheffel@outlook.com>
2025-11-26 10:59:10 -0300

    configurando entidades

--- app-foundations/Models/Estabelecimento.swift
+++ app-foundations/Models/Estabelecimento.swift
@@ -5,11 +5,20 @@
 //  Created by aluno-15 on 25/11/25.
 //
 
-import Foundation
+import CoreLocation
+
 
 struct Estabelecimento: Identifiable {
     let id = UUID()
     let name: String
     let modality: String
     let image: URL?
+    let endereco: String?
+    let avaliacao: Double?
+    let sobre: String?
+    let horario: String?
+    let oferece: String? // enum
+    let latitude: Double?
+    let longitude: Double?
+    let isFavorite: Bool?
 }
---
+++ app-foundations/Models/WorkoutActivityEnumEditado.swift
@@ -0,0 +1,96 @@
+//
+//  WorkoutActivityEnum.swift
+//  Tibby
+//
+//  Edicao do arquivo Created by Felipe Elsner Silva on 25/11/25.
+//
+
+import Foundation
+
+enum WorkoutActivityType: CaseIterable, Codable {
+    case basketball, bowling, golf, gymnastics, handball,martialArts, sailing, outdoorSoccer, indoorSoccer,squash, swimming, tableTennis, tennis, traditionalStrengthTraining, volleyball, highIntensityIntervalTraining,boxing, pilates, stepTraining,equestrianSports,iceSkating, mindAndBody,dance
+    /*
+     Simple mapping of available workout types to a human readable name.
+     */
+    var name: String {
+        switch self {
+        case .basketball:                   return "Basquete"
+        case .bowling:                      return "Boliche"
+        case .boxing:                       return "Boxe"
+        case .dance:                        return "Dança"
+        case .equestrianSports:             return "Equestrian Sports"
+        case .golf:                         return "Golf"
+        case .gymnastics:                   return "Ginástica"
+        case .handball:                     return "Handebol"
+        case .highIntensityIntervalTraining:    return " HIIT High Intensity Interval Training"
+        case .iceSkating:                   return "Patinação"
+        case .indoorSoccer:                return "Futsal"
+        case .martialArts:                  return "Artes Marciais"
+        case .mindAndBody:                  return "Yoga"
+        case .outdoorSoccer:                return "Futebol"
+        case .pilates:                      return "Pilates"
+        case .sailing:                      return "Vela"
+        case .squash:                       return "Squash"
+        case .stepTraining:                 return "Treino Funcional"
+        case .swimming:                     return "Natação"
+        case .tableTennis:                  return "Tênis de Mesa"
+        case .tennis:                       return "Tênis"
+        case .traditionalStrengthTraining:  return "Academia"
+        case .volleyball:                   return "Vôlei"
+        }
+        
+        var icon: String {
+            var icon: String {
+                switch self {
+                case .basketball:
+                    return "figure.basketball"
+                case .bowling:
+                    return "figure.bowling"
+                case .golf:
+                    return "figure.golf"
+                case .gymnastics:
+                    return "figure.gymnastics"
+                case .handball:
+                    return "figure.handball"
+                case .martialArts:
+                    return "figure.martial.arts" 
+                case .sailing:
+                    return "figure.sailing"
+                case .outdoorSoccer:
+                    return "figure.soccer"
+                case .indoorSoccer:
+                    return "figure.soccer"
+                case .squash:
+                    return "figure.squash"
+                case .swimming:
+                    return "figure.pool.swim"
+                case .tableTennis:
+                    return "figure.table.tennis"
+                case .tennis:
+                    return "figure.tennis"
+                case .traditionalStrengthTraining:
+                    return "dumbbell.fill"
+                case .volleyball:
+                    return "figure.volleyball"
+                case .highIntensityIntervalTraining:
+                    return "figure.run"
+                case .boxing:
+                    return "figure.boxing"
+                case .pilates:
+                    return "figure.pilates"
+                case .stepTraining:
+                    return "figure.stairs"
+                case .equestrianSports:
+                    return "figure.equestrian.sports"
+                case .iceSkating:
+                    return "figure.skating"
+                case .mindAndBody:
+                    return "figure.yoga"
+                case .dance:
+                    return "figure.dance"
+                }
+                return "figure."+name
+            }
+        }
+    }
+}
